ff6c4d568fbe3174129cbf645576fd7fd8b7f40a
Dylan <klj40702@gmail.com>
2024-02-27 20:02:59 +0800

    commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  default layout
+  <slot></slot>
+</template>
+
+<script setup>
+const route = useRoute();
+
+const routeId = computed(() => route.params.id);
+
+await useAsyncData(
+  `test_${routeId.value}`,
+  () => $fetch('https://run.mocky.io/v3/b03c800b-77f2-429d-9fe6-55d1bd630ab1'),
+  { watch: [routeId] }
+);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/[id]/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ data?.message }}
+    <button @click="goTo(1)">Go to 1</button>
+    <button @click="goTo(2)">Go to 2</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const router = useRouter();
+
+const routeId = computed(() => route.params.id);
+
+const { data } = useNuxtData(`test_${routeId.value}`);
+
+function goTo(id: number) {
+  router.replace({ path: `/${id}`, query: route.query, hash: route.hash });
+}
+</script>
